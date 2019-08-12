207ecb9e2e0c7513009c29979ec6c79f93c44ade
Gustavo Vergara <gusvego@comunidad.unam.mx>
2019-08-12 16:34:35 -0500

    Create simula_booleano.c

---
+++ eda1_p01/simula_booleano.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef unsigned char booleano;
+
+int main()
+{
+    booleano mio = FALSE;
+    printf( mio ? "si\n" : "no\n" );
+    printf( "%d", sizeof(byte) );
+
+    return 0;
+}
