597cba5399807fc304faa92e8a29d7381a56480a
gus <gus_ya@hotmail.com>
2019-08-11 19:17:21 -0500

    Modificando el msj de salida

--- eda1_p01/datos_primitivos.c
+++ eda1_p01/datos_primitivos.c
@@ -4,12 +4,12 @@
 int main() { 
     char cadena1[] = "cadena";
     char cadena2[] = { 'c', 'a', 'd', 'e', 'n', 'a' };
-    printf("%d\n", sizeof( char ) );
-    printf("%d\n", sizeof( int ) );
-    printf("%d\n", sizeof( float ) );
-    printf("%d\n", sizeof( double ) );
+    printf("Char = %d\n", sizeof( char ) );
+    printf("Int = %d\n", sizeof( int ) );
+    printf("Float = %d\n", sizeof( float ) );
+    printf("Double = %d\n", sizeof( double ) );
     
-    printf("%d\n", sizeof( cadena1 ) );
-    printf("%d", sizeof( cadena2) );
+    printf("cadena1 = %d\n", sizeof( cadena1 ) );
+    printf("cadena2 = %d", sizeof( cadena2) );
     return 0; 
 }
