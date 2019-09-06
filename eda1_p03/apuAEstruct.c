9dadf189d6b7299db72c0dcbd9c98e0e7b95ba15
Gustavo Vergara <gusvego@comunidad.unam.mx>
2019-09-06 16:43:25 -0500

    Create apuAEstruct.c

---
+++ eda1_p03/apuAEstruct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct{
+  char *cara;
+  char *palo;
+} Carta;
+
+int main()
+{
+  Carta unaCarta;
+  Carta *ptrCarta;
+  unaCarta.cara = "As";
+  unaCarta.palo = "Copas";
+  ptrCarta = &unaCarta;
+
+  printf("%s de %s\n", unaCarta.cara, unaCarta.palo);
+  printf("%s de %s\n", ptrCarta->cara, ptrCarta->palo);
+  printf("%s de %s\n", ( *ptrCarta ).cara, (*ptrCarta ).palo);
+
+  return 0;
+}
