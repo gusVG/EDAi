6dad4d7a46d37b3ce734a24dc2de2b5ef40e4abc
Gustavo Vergara <gusvego@comunidad.unam.mx>
2019-09-06 16:42:19 -0500

    Estructura Rectángulo
    Estructura dentro de estructura, pase como argumento y devolución de función

--- eda1_p03/typedefEj.c
+++ eda1_p03/typedefEj.c
@@ -1,26 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct estudiante {
-   char *nombre;
-   char *apPaterno;
-   char *apMaterno;
-};
-//El nuevo tipo de dato definido se
-//llama: struct estudiante
+typedef struct {
+    int x;
+    int y;
+    char* identificador;
+} Punto;
 
-typedef struct estudiante elestudiante;
+typedef struct {
+    Punto puntos[4];
+    int perimetro;
+    int area;
+} Rectangulo;
 
-int main( ) {
-    elestudiante alumno2;
+int calculaArea(int, int);
+int calculaPerimetro(int, int);
 
-    alumno2.nombre = "Mengano";
-    alumno2.apPaterno = "DePadre";
-    alumno2.apMaterno = "YDeMadre";
+// esta funcion me devuelve los puntos a calcular
+Punto calculaPuntoSupDer(Punto, Punto);
+Punto calculaPuntoInfIzq(Punto, Punto);
 
-    printf("Todo: %s %s %s",
-		alumno2.nombre,
-		alumno2.apPaterno,
-		alumno2.apMaterno);
+int main(){
+    Rectangulo miRectangulo;
+    Rectangulo* apuRect;
+    int i, base, altura;
 
-   return 0;
+    apuRect = &miRectangulo;
+
+    printf("Dame X1: ");
+    scanf("%d", &miRectangulo.puntos[0].x);
+    printf("\nDame Y1: ");
+    scanf("%d", &miRectangulo.puntos[0].y);
+    printf("\nDame X2: ");
+    scanf("%d", &miRectangulo.puntos[2].x);
+    printf("\nDame Y2: ");
+    scanf("%d", &miRectangulo.puntos[2].y);
+
+    miRectangulo.puntos[1] = calculaPuntoSupDer(miRectangulo.puntos[0], miRectangulo.puntos[2]);
+    miRectangulo.puntos[3] = calculaPuntoInfIzq(miRectangulo.puntos[0], miRectangulo.puntos[2]);
+
+    base = miRectangulo.puntos[2].x - miRectangulo.puntos[0].x;
+    altura = miRectangulo.puntos[2].y - miRectangulo.puntos[0].y;
+
+    miRectangulo.area = base * altura;
+    miRectangulo.perimetro = 2 * base + 2 * altura;
+
+    system("cls");
+    for(i=0; i<4; i++) {
+      printf("Punto %d: (%d, %d)\n", i+1, apuRect->puntos[i].x, apuRect->puntos[i].y);
+    }
+
+    printf("\nArea = %d: ", apuRect->area);
+    printf("\nPerimetro = %d: ", apuRect->perimetro);
+
+    return 0;
+}
+
+
+Punto calculaPuntoSupDer(Punto pun1, Punto pun2){
+    Punto resultante;
+    resultante.x = pun2.x;
+    resultante.y = pun1.y;
+
+    return resultante;
+}
+
+Punto calculaPuntoInfIzq(Punto pun1, Punto pun2){
+    Punto resultante;
+    resultante.x = pun1.x;
+    resultante.y = pun2.y;
+
+    return resultante;
 }
